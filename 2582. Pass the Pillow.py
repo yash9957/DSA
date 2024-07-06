da1a4892fa066c476313a3dd025fd614e4b9e16f
Yash Wairagade <yashwairagade99@gmail.com>
2024-07-06 20:38:03 +0530

    new file

---
+++ 2582. Pass the Pillow.py
@@ -0,0 +1,12 @@
+#There are n people standing in a line labeled from 1 to n. The first person in the line is holding a pillow initially. Every second, the person holding the pillow passes it to the next person standing in the line. Once the pillow reaches the end of the line, the direction changes, and people continue passing the pillow in the opposite direction.
+#For example, once the pillow reaches the nth person they pass it to the n - 1th person, then to the n - 2th person and so on.
+#Given the two positive integers n and time, return the index of the person holding the pillow after time seconds.
+
+class Solution:
+    def passThePillow(self, n: int, time: int) -> int:
+        cycle_length = 2 * (n - 1)
+        time = time % cycle_length
+        if time < n:
+            return time + 1
+        else:
+            return 2 * n - time - 1

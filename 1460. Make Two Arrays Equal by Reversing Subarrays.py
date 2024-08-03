056ccc7fb1239d22a48ef05d4cde4a3eb2c97c6e
Yash Wairagade <yashwairagade99@gmail.com>
2024-08-03 12:37:46 +0530

    new file

--- 1460. Make Two Arrays Equal by Reversing Subarrays.py
+++ 1460. Make Two Arrays Equal by Reversing Subarrays.py
@@ -4,3 +4,29 @@ Return true if you can make arr equal to target or false otherwise.'''
 class Solution:
     def canBeEqual(self, target: List[int], arr: List[int]) -> bool:
         return sorted(target) == sorted(arr)
+
+
+
+
+class Solution:
+    def canBeEqual(self, target: List[int], arr: List[int]) -> bool:
+        def quicksort(array: List[int]) -> List[int]:
+            if len(array) <= 1:
+                return array
+            pivot = array[len(array) // 2]
+            left = [x for x in array if x < pivot]
+            middle = [x for x in array if x == pivot]
+            right = [x for x in array if x > pivot]
+            return quicksort(left) + middle + quicksort(right)
+
+        sorted_target = quicksort(target)
+        sorted_arr = quicksort(arr)
+
+        if len(sorted_target) != len(sorted_arr):
+            return False
+
+        for i in range(len(sorted_target)):
+            if sorted_target[i] != sorted_arr[i]:
+                return False
+
+        return True

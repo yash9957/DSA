3e410d59088a6b36d59de63b0ef7b2d882e857c8
Yash Wairagade <yashwairagade99@gmail.com>
2024-08-06 11:36:41 +0530

    new file

---
+++ 3016. Minimum Number of Pushes to Type Word II.py
@@ -0,0 +1,16 @@
+'''You are given a string word containing lowercase English letters.
+Telephone keypads have keys mapped with distinct collections of lowercase English letters, which can be used to form words by pushing them. For example, the key 2 is mapped with ["a","b","c"], we need to push the key one time to type "a", two times to type "b", and three times to type "c" .
+It is allowed to remap the keys numbered 2 to 9 to distinct collections of letters. The keys can be remapped to any amount of letters, but each letter must be mapped to exactly one key. You need to find the minimum number of times the keys will be pushed to type the string word.
+Return the minimum number of pushes needed to type word after remapping the keys.
+An example mapping of letters to keys on a telephone keypad is given below. Note that 1, *, #, and 0 do not map to any letters.'''
+
+
+
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        freqs = Counter(word)
+        sorted_freqs = sorted(freqs.items(), key=lambda x: x[1], reverse=True)
+        presses = 0
+        for i , (letters, freq) in enumerate(sorted_freqs):
+            presses += (i // 8 + 1) * freq
+        return presses
